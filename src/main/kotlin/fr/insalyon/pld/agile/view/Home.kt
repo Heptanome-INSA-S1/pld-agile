4060deccb2440cffae98f273958546dd462f60ad
Loic R <rouquette.loic@gmail.com>
2017-11-10 15:53:55 +0100

    Add fxtornado example

---
+++ src/main/kotlin/fr/insalyon/pld/agile/view/Home.kt
@@ -0,0 +1,27 @@
+package fr.insalyon.pld.agile.view
+
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.scene.control.Label
+import javafx.scene.layout.BorderPane
+import tornadofx.*
+
+/**
+ * Default home screen
+ */
+class Home : View() {
+    // Map the current view to resources/view/Home.fxml
+    override val root : BorderPane by fxml("/view/Home.fxml")
+    // Create an integer property
+    val counter = SimpleIntegerProperty()
+    // Create a label which will display the value
+    val counterLabel: Label by fxid()
+
+    init {
+        // Bind the value of label to the property counter
+        counterLabel.bind(counter)
+    }
+
+    fun increment() {
+        counter.value += 1
+    }
+}
