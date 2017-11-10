3018668ba37644b47340709ba954601048338a4f
Loic R <rouquette.loic@gmail.com>
2017-11-10 16:05:02 +0100

    Add alternative way to create a tornadox fx view

--- src/main/kotlin/fr/insalyon/pld/agile/App.kt
+++ src/main/kotlin/fr/insalyon/pld/agile/App.kt
@@ -1,10 +1,11 @@
 package fr.insalyon.pld.agile
 
+import fr.insalyon.pld.agile.view.AlternativeHome
 import fr.insalyon.pld.agile.view.Home
 import javafx.application.Application
 
 // Create the default start application
-class App: tornadofx.App(Home::class)
+class App: tornadofx.App(AlternativeHome::class)
 
 fun main(args: Array<String>) {
     // Start the application
