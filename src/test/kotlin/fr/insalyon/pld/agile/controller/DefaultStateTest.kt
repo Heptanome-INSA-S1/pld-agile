3ead62671bcc71583c92d6835115f89c3ec43352
ThomasPoint <pointthomas84700@gmail.com>
2017-11-17 13:48:06 +0100

    update test for loadMap

--- src/test/kotlin/fr/insalyon/pld/agile/controller/DefaultStateTest.kt
+++ src/test/kotlin/fr/insalyon/pld/agile/controller/DefaultStateTest.kt
@@ -3,15 +3,40 @@ package fr.insalyon.pld.agile.controller
 import com.sun.media.sound.InvalidFormatException
 import fr.insalyon.pld.agile.controller.implementation.DefaultState
 import org.junit.Test
+import java.io.File
+import java.io.FileNotFoundException
+import java.nio.file.Path
+import java.nio.file.Paths
 
 class DefaulStateTest {
 
     /**
-     * File without xml extension
+     * Try to load a plan from a non xml file
      */
     @Test(expected = InvalidFormatException::class)
     fun loadPlanInvalidFormatException(){
         val DEFAULT_STATE = DefaultState()
-        DEFAULT_STATE.loadPlan("view/Home.fxml")
+        DEFAULT_STATE.loadPlan(System.getProperty("user.dir")+"\\src\\test\\resources\\test.txt")
     }
+
+    /**
+     * Try to load a plan from a non existing file
+     */
+    @Test(expected = FileNotFoundException::class)
+    fun loadPlanNonExistingFile(){
+        val DEFAULT_STATE = DefaultState()
+        DEFAULT_STATE.loadPlan(System.getProperty("user.dir")+"\\src\\test\\resources\\fichiersXML\\PlanXML\\planLyonGran.xml")
+    }
+
+    /**
+     * Try to load an xml file which is not correctly formatted
+     */
+    @Test(expected = InvalidFormatException::class)
+    fun loadPlanXMLInvalidFormat(){
+        val DEFAULT_STATE = DefaultState()
+        DEFAULT_STATE.loadPlan(System.getProperty("user.dir")+"\\src\\test\\resources\\fichiersXML\\PlanXML\\planLyonSansNoeud.xml")
+    }
+
+
+
 }
