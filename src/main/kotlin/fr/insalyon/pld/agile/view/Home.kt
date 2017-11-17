f955cb3a91e1c4c72b3fe29ce16e7114ce47915a
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2017-11-17 10:49:19 +0100

    First version not responsive

--- src/main/kotlin/fr/insalyon/pld/agile/view/Home.kt
+++ src/main/kotlin/fr/insalyon/pld/agile/view/Home.kt
@@ -11,17 +11,17 @@ import tornadofx.*
 class Home : View() {
   // Map the current view to resources/view/Home.fxml
   override val root: BorderPane by fxml("/view/Home.fxml")
-  // Create an integer property
+  /*// Create an integer property
   val counter = SimpleIntegerProperty()
   // Create a label which will display the value. Match with label fx:id="counterLabel"
-  val counterLabel: Label by fxid()
+  val counterLabel: Label by fxid()*/
 
   init {
     // Bind the value of label to the property counter
-    counterLabel.bind(counter)
+    //counterLabel.bind(counter)
   }
 
   fun increment() {
-    counter.value += 1
+    //counter.value += 1
   }
 }
