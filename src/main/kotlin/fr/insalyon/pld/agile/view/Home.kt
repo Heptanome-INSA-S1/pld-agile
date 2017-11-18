9b492fbe781d67e28782a07a07b2904e5bda1e9e
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2017-11-18 12:59:24 +0100

    First version of the UI

--- src/main/kotlin/fr/insalyon/pld/agile/view/Home.kt
+++ src/main/kotlin/fr/insalyon/pld/agile/view/Home.kt
@@ -3,6 +3,7 @@ package fr.insalyon.pld.agile.view
 import javafx.beans.property.SimpleIntegerProperty
 import javafx.scene.control.Label
 import javafx.scene.layout.BorderPane
+import javafx.scene.layout.GridPane
 import tornadofx.*
 
 /**
