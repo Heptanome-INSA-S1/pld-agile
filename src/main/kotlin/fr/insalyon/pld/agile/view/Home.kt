3695560981509f968ededbacfade00d9d2b43fe5
Justin GALLY <justin.gallysp@gmail.com>
2017-11-19 16:04:41 +0100

    PlanFragment v1

--- src/main/kotlin/fr/insalyon/pld/agile/view/Home.kt
+++ src/main/kotlin/fr/insalyon/pld/agile/view/Home.kt
@@ -1,5 +1,12 @@
 package fr.insalyon.pld.agile.view
 
+
+import fr.insalyon.pld.agile.model.Plan
+import fr.insalyon.pld.agile.util.xml.XmlDocument
+import fr.insalyon.pld.agile.util.xml.serialization.implementation.IntersectionSerializer
+import fr.insalyon.pld.agile.util.xml.serialization.implementation.JunctionSerializer
+import fr.insalyon.pld.agile.util.xml.serialization.implementation.PlanSerializer
+import fr.insalyon.pld.agile.view.fragments.PlanFragment
 import javafx.beans.property.SimpleIntegerProperty
 import javafx.scene.control.Label
 import javafx.scene.layout.BorderPane
@@ -10,18 +17,34 @@ import tornadofx.*
  */
 class Home : View() {
   // Map the current view to resources/view/Home.fxml
-  override val root: BorderPane by fxml("/view/Home.fxml")
-  // Create an integer property
-  val counter = SimpleIntegerProperty()
-  // Create a label which will display the value. Match with label fx:id="counterLabel"
-  val counterLabel: Label by fxid()
+    override val root: BorderPane by fxml("/view/Home.fxml")
+    // Create an integer property
+    val counter = SimpleIntegerProperty()
+    // Create a label which will display the value. Match with label fx:id="counterLabel"
+    val counterLabel: Label by fxid()
+    val plan: Plan
 
-  init {
-    // Bind the value of label to the property counter
-    counterLabel.bind(counter)
-  }
+    init {
+        // Bind the value of label to the property counter
+        counterLabel.bind(counter)
 
-  fun increment() {
-    counter.value += 1
-  }
+
+        val xmlPlan = XmlDocument.open("C:/Users/Ordinateur/Documents/INSA/4IF/PLD AGILE/A.G.I.L.E Project/src/main/resources/xml/planLyonGrand.xml")
+
+        val intersectionSerializer = IntersectionSerializer(xmlPlan)
+        val junctionSerializer = JunctionSerializer(xmlPlan)
+
+        val planSerializer = PlanSerializer(xmlPlan, intersectionSerializer, junctionSerializer)
+        plan = planSerializer.unserialize(xmlPlan.documentElement)
+
+    }
+
+    fun increment() {
+        counter.value += 1
+        //replaceWith(PlanFragment::class)
+    }
+
+    fun planView() {
+        replaceWith(find<PlanFragment>(mapOf(PlanFragment::plan to plan)))
+    }
 }
---
+++ src/main/kotlin/fr/insalyon/pld/agile/view/fragments/PlanFragment.kt
@@ -0,0 +1,69 @@
+package fr.insalyon.pld.agile.view.fragments
+
+import fr.insalyon.pld.agile.model.Plan
+import javafx.scene.paint.Color
+import tornadofx.*
+
+class PlanFragment : Fragment(){
+    val plan : Plan by param()
+
+    var X_SIZE = 1000.0
+    var Y_SIZE = 1000.0
+    var SIZE = 1.0
+
+
+    override val root = vbox {
+        val stack = stackpane {
+            scrollpane {
+                println("Test")
+                group {
+                    plan.nodes.forEach{
+                        val nodeX : Double = it.element.x/(plan.width*1.0) * X_SIZE
+                        val nodeY : Double = it.element.y/(plan.height*1.0) * Y_SIZE
+                        circle {
+                            centerX = nodeX
+                            centerY = nodeY
+                            radius = SIZE
+                            if (it.element.id == 1L)
+                                fill=Color.RED
+                        }
+                        plan.outEdges[it.index].forEach{
+                            line{
+                                startX = nodeX
+                                startY = nodeY
+                                endX = it.to.element.x/(plan.width*1.0) * X_SIZE
+                                endY = it.to.element.y/(plan.height*1.0) * Y_SIZE
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        /*hbox {
+            button("Zoom +"){
+                action {
+                    X_SIZE += 100.0
+                    Y_SIZE += 100.0
+                    println("X : "+X_SIZE+" Y: "+Y_SIZE)
+
+                    stack.
+
+
+                }
+            }
+            button("Zoom -"){
+                action{
+                    X_SIZE -= 10.00
+                    Y_SIZE -= 100.0
+                    println("X : "+X_SIZE+" Y: "+Y_SIZE)
+                }
+            }
+        }
+        hbox {
+            slider(0.0, 1000.0){
+
+            }
+        }*/
+    }
+
+}
