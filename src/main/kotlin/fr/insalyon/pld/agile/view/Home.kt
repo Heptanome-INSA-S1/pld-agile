1ee0fc1b653c1da24a3f30930341954f006932f3
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2017-11-19 16:44:43 +0100

    First version Home + Map

--- src/main/kotlin/fr/insalyon/pld/agile/view/Home.kt
+++ src/main/kotlin/fr/insalyon/pld/agile/view/Home.kt
@@ -8,27 +8,40 @@ import fr.insalyon.pld.agile.util.xml.serialization.implementation.JunctionSeria
 import fr.insalyon.pld.agile.util.xml.serialization.implementation.PlanSerializer
 import fr.insalyon.pld.agile.view.fragments.PlanFragment
 import javafx.beans.property.SimpleIntegerProperty
+import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.GridPane
+import javafx.scene.layout.VBox
 import tornadofx.*
 
 /**
  * Default home screen
  */
 class Home : View() {
-  // Map the current view to resources/view/Home.fxml
+    override val root: BorderPane by fxml("/view/Home.fxml")
+    // Map the current view to resources/view/Home.fxml
     val plan: Plan
+    val loadPlanButton: Button by fxid();
+    val centerVBox: VBox by fxid();
 
     init {
 
-        val xmlPlan = XmlDocument.open("C:/Users/Ordinateur/Documents/INSA/4IF/PLD AGILE/A.G.I.L.E Project/src/main/resources/xml/planLyonGrand.xml")
+        val xmlPlan = XmlDocument.open("src/main/resources/xml/planLyonGrand.xml")
         val intersectionSerializer = IntersectionSerializer(xmlPlan)
         val junctionSerializer = JunctionSerializer(xmlPlan)
         val planSerializer = PlanSerializer(xmlPlan, intersectionSerializer, junctionSerializer)
         plan = planSerializer.unserialize(xmlPlan.documentElement)
+
+        loadPlanButton.setOnAction {
+            planView()
+        }
     }
     fun planView() {
-        replaceWith(find<PlanFragment>(mapOf(PlanFragment::plan to plan)))
+        root.center{
+            add(PlanFragment::class, mapOf(PlanFragment::plan to plan))
+            //replaceWith(find<PlanFragment>(PlanFragment::class, plan))
+            //centerVBox.replaceWith(find<PlanFragment>(mapOf(PlanFragment::plan to plan)))
+        }
     }
 }
