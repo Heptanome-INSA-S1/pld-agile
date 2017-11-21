e45d640bf83c9d1b51e2f065770927a8c6e4fd90
tgay <Tkd-thomas-971@hotmail.fr>
2017-11-21 08:46:29 +0100

    Create branch RoadSheetV2 and package util.txt

---
+++ src/main/kotlin/fr/insalyon/pld/agile/util/txt/RoadSheetSerializer.kt
@@ -0,0 +1,11 @@
+package fr.insalyon.pld.agile.util.txt
+
+import fr.insalyon.pld.agile.model.*
+import org.w3c.dom.Element
+
+class RoadSheetSerializer(val round: Round){
+
+    fun serialize(element: Round){
+
+    }
+}
