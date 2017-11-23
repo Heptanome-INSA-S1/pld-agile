bc010dfa0095cf1d9b8899218dff7ae5224b0e85
ThomasPoint <pointthomas84700@gmail.com>
2017-11-23 14:11:27 +0100

    some modifications in the road sheet file the file output is located in the resources folder

--- src/main/kotlin/fr/insalyon/pld/agile/util/txt/RoadSheetSerializer.kt
+++ src/main/kotlin/fr/insalyon/pld/agile/util/txt/RoadSheetSerializer.kt
@@ -1,5 +1,6 @@
 package fr.insalyon.pld.agile.util.txt
 
+import fr.insalyon.pld.agile.getResource
 import fr.insalyon.pld.agile.model.*
 import java.io.File
 
@@ -12,32 +13,37 @@ class RoadSheetSerializer(){
 
     private fun getStartTime(sT: Instant?):String{
         if(sT!=null)return "Start Time = ${sT}"
-        return ""
+        return "n/a"
     }
 
     private fun getEndTime(eT: Instant?): String{
         if(eT!=null) return "End Time = ${eT}"
-        return ""
+        return "n/a"
     }
 
     fun serialize(round: Round) {
-        if(File("RoadSheet.txt").exists()) File("RoadSheet.txt").delete()
-        roadSheet = File("RoadSheet.txt")
+        if(File(getResource("RoadSheet.txt").path).exists()) File(getResource("RoadSheet.txt").path).delete()
+        roadSheet = File(getResource("RoadSheet.txt").path)
 
-        var warehouseDescription: String = "Warehouse : ${round.warehouse.address.x} ${round.warehouse.address.y} - Departure Hour = ${round.warehouse.departureHour} "
-        append(warehouseDescription+"\r\n____________________________________\r\n")
+        append("Beginning of the round ! \n")
+        var warehouseDescription: String = "You have to leave the warehouse (x:${round.warehouse.address.x}, y:${round.warehouse.address.y}) at ${round.warehouse.departureHour} "
+        append(warehouseDescription+"\r\n________________________________________________________________________\r\n")
 
         //round.deliveries.forEach { d -> append("Delivery : x = ${d.address.x} y = ${d.address.y} Start Time = ${d.startTime} End Time = ${d.endTime} Duration = ${d.duration.length}") }
         var j=0
         for(i in 0 until round.deliveries.size){
+            append("You have to : \n\n")
             while(j<round.path.size && round.deliveries.elementAt(i).address != round.path.elementAt(j).nodes[0]) {
-                round.path.elementAt(j).edges.forEach { e -> append("${e.name} \r\n")}
+                round.path.elementAt(j).edges.forEach { e -> append("\t|Take ${e.name} during ${e.length}\r\n")}
                 j++
             }
-            append("Delivery $i : x = ${round.deliveries.elementAt(i).address.x} y = ${round.deliveries.elementAt(i).address.y} ${getStartTime(round.deliveries.elementAt(i).startTime)}${getEndTime(round.deliveries.elementAt(i).endTime)}Duration = ${round.deliveries.elementAt(i).duration.length}")
-            append("\r\n____________________________________\r\n")
+            append("\r\n\tIt's now time to do the delivery $i (x:${round.deliveries.elementAt(i).address.x}, y:${round.deliveries.elementAt(i).address.y}) - Details:")
+            append("\n\t\tStart at : ${getStartTime(round.deliveries.elementAt(i).startTime)}")
+            append("\n\t\tFinish at : ${getEndTime(round.deliveries.elementAt(i).endTime)}")
+            append("\n\t\tDuration = ${round.deliveries.elementAt(i).duration.length}")
+            append("\r\n________________________________________________________________________\r\n")
         }
-        round.path.elementAt(j).edges.forEach { e -> append("${e.name} \r\n")}
-        append("Warehouse : x = ${round.warehouse.address.x} y = ${round.warehouse.address.y} \r\nFIN TOURNEE")
+        append("It's nearly finished, to come back at the warehouse : \n\n")
+        round.path.elementAt(j).edges.forEach { e -> append("\t|Take ${e.name} during ${e.length}\r\n")}
     }
 }
