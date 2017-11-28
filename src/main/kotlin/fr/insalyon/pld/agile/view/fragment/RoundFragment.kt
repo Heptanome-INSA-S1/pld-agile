009b13bac07ac73b32fe6b7d1810414ee7330bc9
Unknown <gonnet.tim@gmail.com>
2017-11-28 08:51:46 +0100

    Add button for futur modifications

--- src/main/kotlin/fr/insalyon/pld/agile/view/fragment/RoundFragment.kt
+++ src/main/kotlin/fr/insalyon/pld/agile/view/fragment/RoundFragment.kt
@@ -5,11 +5,11 @@ import fr.insalyon.pld.agile.model.Round
 import fr.insalyon.pld.agile.view.event.HighlightLocationEvent
 import fr.insalyon.pld.agile.view.event.HighlightLocationInListEvent
 import javafx.scene.control.Button
-import javafx.scene.layout.BorderPane
-import javafx.scene.layout.HBox
+import javafx.scene.layout.*
 import javafx.scene.paint.Color
 import javafx.scene.text.FontWeight
 import tornadofx.*
+import java.net.URI
 
 class RoundFragment : Fragment() {
   val parentView: BorderPane by param()
@@ -26,7 +26,7 @@ class RoundFragment : Fragment() {
       }
     }
     hbox {
-      paddingTop=2
+      paddingTop=10.0
       label ("1.  "){
         paddingTop=4
       }
@@ -43,33 +43,94 @@ class RoundFragment : Fragment() {
         paddingTop=4
       }
     }
-    label("")
-    label("Deliveries") {
-      paddingLeft=30.0
-      style{
-        fontWeight = FontWeight.BOLD
+    borderpane {
+      paddingTop=20.0
+      minWidth=350.0
+      center{
+        vbox{
+          label("Deliveries") {
+            paddingTop=5.0
+            prefHeight=30.0
+            paddingBottom=15.0
+            paddingLeft=30.0
+            style{
+              fontWeight = FontWeight.BOLD
+            }
+          }
+          for (i in round!!.deliveries().indices) {
+            hbox {
+              paddingTop=2
+              label ("${i+2}.${if(i+2<10) "  " else ""}"){
+                paddingTop=4
+              }
+              button(""+round!!.deliveries().elementAt(i).address.id){
+                id = ""+round!!.deliveries().elementAt(i).address.id
+                action{
+                  fire(HighlightLocationEvent(""+round!!.deliveries().elementAt(i).address.id,false))
+                }
+                style{
+                  baseColor=Color.WHITE
+                }
+              }
+              label (deliveryToText(round!!.deliveries().elementAt(i))){
+                paddingTop=4
+              }
+            }
+          }
+        }
+      }
+      right{
+        vbox{
+          hbox {
+            spacing=3.0
+            paddingBottom=10.0
+            button{
+              prefWidth=30.0
+              prefHeight=30.0
+              style{
+                backgroundImage += URI.create( "image/add.png")
+                backgroundRepeat += Pair(BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT)
+                backgroundPosition+= BackgroundPosition.CENTER
+              }
+            }
+            button{
+              prefWidth=30.0
+              prefHeight=30.0
+              style{
+                backgroundImage += URI.create( "image/undo.png")
+                backgroundRepeat += Pair(BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT)
+                backgroundPosition+= BackgroundPosition.CENTER
+              }
+            }
+          }
+          for (i in round!!.deliveries().indices) {
+            hbox {
+              spacing=3.0
+              paddingTop=2
+              button{
+                prefWidth=30.0
+                prefHeight=30.0
+                style{
+                  backgroundImage += URI.create( "image/edit.png")
+                  backgroundRepeat += Pair(BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT)
+                  backgroundPosition+= BackgroundPosition.CENTER
+                }
+              }
+              button{
+                prefWidth=30.0
+                prefHeight=30.0
+                style{
+                  backgroundImage += URI.create( "image/delete.png")
+                  backgroundRepeat += Pair(BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT)
+                  backgroundPosition+= BackgroundPosition.CENTER
+                }
+              }
+            }
+          }
+        }
       }
     }
-    for (i in round!!.deliveries().indices) {
-      hbox {
-        paddingTop=2
-        label ("${i+2}.${if(i+2<10) "  " else ""}"){
-          paddingTop=4
-        }
-        button(""+round!!.deliveries().elementAt(i).address.id){
-          id = ""+round!!.deliveries().elementAt(i).address.id
-          action{
-            fire(HighlightLocationEvent(""+round!!.deliveries().elementAt(i).address.id,false))
-          }
-          style{
-            baseColor=Color.WHITE
-          }
-        }
-        label (deliveryToText(round!!.deliveries().elementAt(i))){
-          paddingTop=4
-        }
-      }
-    }
+
     label("")
     label("Warehouse") {
       paddingLeft=30.0
@@ -78,7 +139,7 @@ class RoundFragment : Fragment() {
       }
     }
     hbox {
-      paddingTop=2
+      paddingTop=10.0
       label ("${round!!.deliveries().size+2}.${if(round!!.deliveries().size+2<10) "  " else ""}"){
         paddingTop=4
       }
