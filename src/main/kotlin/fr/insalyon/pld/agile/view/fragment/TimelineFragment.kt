42e75e3f63e5c3a41c133a96e83e26b55bf418ac
Tim <gonnet.tim@gmail.com>
2017-12-01 10:51:12 +0100

    Remove time if to close, bottom view

--- src/main/kotlin/fr/insalyon/pld/agile/view/fragment/TimelineFragment.kt
+++ src/main/kotlin/fr/insalyon/pld/agile/view/fragment/TimelineFragment.kt
@@ -57,7 +57,7 @@ class TimelineFragment() : Fragment() {
     }
     actualX=15.0
     round.distancePathInMeters().forEachIndexed { index,it1 ->
-      val deplacement = transform(it1.edges)
+      var deplacement = transform(it1.edges)
       actualX += deplacement
       time += length(it1.edges).seconds.toSeconds()
       val idDelivery = it1.nodes.last().id.toString()
@@ -75,24 +75,27 @@ class TimelineFragment() : Fragment() {
           }
         }
       }
-      label("${secondsToString(time)}"){
-        id=idDelivery
-        layoutX=actualX-10.0
-        layoutY=middle-23
-        style{
-          fontSize=10.px
-        }
-      }
-      if(index<round.deliveries().size){
-        time+= round.deliveries().first { it.address.id.toString() == idDelivery }.duration.toSeconds()
+      if(deplacement>30)
         label("${secondsToString(time)}"){
-          id=it1.nodes.last().id.toString()
+          id=idDelivery
           layoutX=actualX-10.0
-          layoutY=middle+10
+          layoutY=middle-23
           style{
             fontSize=10.px
           }
         }
+      if(index<round.deliveries().size){
+        val attente = round.deliveries().first { it.address.id.toString() == idDelivery }.duration.toSeconds().toDouble()
+        time+=attente.toLong()
+        if(transform(round.distancePathInMeters().elementAt(index+1).edges)>30)
+          label("${secondsToString(time)}"){
+            id=it1.nodes.last().id.toString()
+            layoutX=actualX-10.0
+            layoutY=middle+10
+            style{
+              fontSize=10.px
+            }
+          }
       }
     }
     stackpane {
